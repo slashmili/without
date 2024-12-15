3f97fab86ccb10a4f40c97e56483fae73cf3380a
Milad Rastian <miladrastian@gmail.com>
2024-12-15 01:19:30 +0100

    first version

--- lib/without.ex
+++ lib/without.ex
@@ -1,18 +1,47 @@
 defmodule Without do
-  @moduledoc """
-  Documentation for `Without`.
-  """
+  defstruct [:value, :result, :assigns]
 
-  @doc """
-  Hello world.
+  def finit({:ok, value}), do: %Without{value: value, result: :ok, assigns: %{}}
+  def finit({:error, value}), do: %Without{value: value, result: :error, assigns: %{}}
+  def finit(value), do: %Without{value: value, result: :ok, assigns: %{}}
 
-  ## Examples
+  def fresult(%Without{result: result, value: value}) do
+    {result, value}
+  end
 
-      iex> Without.hello()
-      :world
+  def fmap_ok(%Without{result: :error} = context, _func) do
+    context
+  end
 
-  """
-  def hello do
-    :world
+  def fmap_ok(%Without{} = context, func) when is_function(func, 0) do
+    case func.() do
+      {:ok, value} -> %{context | result: :ok, value: value}
+      {:error, error} -> %{context | result: :error, value: error}
+    end
+  end
+
+  def fmap_ok(%Without{} = context, func) when is_function(func, 1) do
+    case func.(context.value) do
+      {:ok, value} -> %{context | result: :ok, value: value}
+      {:error, error} -> %{context | result: :error, value: error}
+    end
+  end
+
+  def fmap_error(%Without{result: :ok} = context, _func) do
+    context
+  end
+
+  def fmap_error(%Without{} = context, func) when is_function(func, 0) do
+    case func.() do
+      {:ok, value} -> %{context | result: :ok, value: value}
+      {:error, error} -> %{context | result: :error, value: error}
+    end
+  end
+
+  def fmap_error(%Without{} = context, func) when is_function(func, 1) do
+    case func.(context.value) do
+      {:ok, value} -> %{context | result: :ok, value: value}
+      {:error, error} -> %{context | result: :error, value: error}
+    end
   end
 end
--- test/without_test.exs
+++ test/without_test.exs
@@ -2,7 +2,107 @@ defmodule WithoutTest do
   use ExUnit.Case
   doctest Without
 
-  test "greets the world" do
-    assert Without.hello() == :world
+  describe "init/1" do
+    test "returns Without struct when value is not a result tuple" do
+      assert %Without{result: :ok, value: "foo"} = Without.finit("foo")
+    end
+
+    test "returns the same value of input is an ok tuple" do
+      assert %Without{result: :ok, value: "foo"} = Without.finit({:ok, "foo"})
+    end
+
+    test "returns the same value of input is an error tuple" do
+      assert %Without{result: :error, value: :oops} = Without.finit({:error, :oops})
+    end
+  end
+
+  describe "fresult/1" do
+    test "converts :ok result struct to final result format" do
+      assert {:ok, "foo"} = {:ok, "foo"} |> Without.finit() |> Without.fresult()
+    end
+
+    test "converts :error result struct to final result format" do
+      assert {:error, :oops} = {:error, :oops} |> Without.finit() |> Without.fresult()
+    end
+  end
+
+  describe "fmap_ok/2" do
+    test "skips executing when input is error tuple" do
+      assert {:error, :oops} =
+               {:error, :oops}
+               |> Without.finit()
+               |> Without.fmap_ok(fn -> {:ok, "what"} end)
+               |> Without.fresult()
+    end
+
+    test "executes when previous result is ok and input function has arity of 0" do
+      assert {:ok, "bar"} =
+               {:ok, "foo"}
+               |> Without.finit()
+               |> Without.fmap_ok(fn -> {:ok, "bar"} end)
+               |> Without.fresult()
+    end
+
+    test "executes when previous result is ok and input function has arity of 1" do
+      assert {:ok, 16} =
+               2
+               |> Without.finit()
+               |> Without.fmap_ok(fn value -> {:ok, value * 8} end)
+               |> Without.fresult()
+    end
+
+    test "returns error when fmap_ok result is error" do
+      assert {:error, :no_match} =
+               "foo"
+               |> Without.finit()
+               |> Without.fmap_ok(fn arg ->
+                 if arg == "bar" do
+                   {:ok, :found_match}
+                 else
+                   {:error, :no_match}
+                 end
+               end)
+               |> Without.fresult()
+    end
+  end
+
+  describe "fmap_error/2" do
+    test "skips executing when input is ok tuple" do
+      assert {:ok, "foo"} =
+               {:ok, "foo"}
+               |> Without.finit()
+               |> Without.fmap_error(fn -> {:error, :oops} end)
+               |> Without.fresult()
+    end
+
+    test "executes when previous result is error and input function has arity of 0" do
+      assert {:error, :file_not_found} =
+               {:error, :oops}
+               |> Without.finit()
+               |> Without.fmap_error(fn -> {:error, :file_not_found} end)
+               |> Without.fresult()
+    end
+
+    test "executes when previous result is ok and input function has arity of 1" do
+      assert {:error, [:wrong, :oops]} =
+               {:error, :oops}
+               |> Without.finit()
+               |> Without.fmap_error(fn value -> {:error, [:wrong, value]} end)
+               |> Without.fresult()
+    end
+
+    test "returns ok when fmap_error result is ok" do
+      assert {:ok, :its_ok} =
+               {:error, :oops}
+               |> Without.finit()
+               |> Without.fmap_error(fn error_msg ->
+                 if error_msg == :oops do
+                   {:ok, :its_ok}
+                 else
+                   {:error, :really_bad}
+                 end
+               end)
+               |> Without.fresult()
+    end
   end
 end
