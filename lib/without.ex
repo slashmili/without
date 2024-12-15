e32b6c0f6048cf8b1982991f6e7869219ef02891
Milad Rastian <miladrastian@gmail.com>
2024-12-15 02:19:45 +0100

    add typespec

--- lib/without.ex
+++ lib/without.ex
@@ -1,14 +1,26 @@
 defmodule Without do
   defstruct [:value, :result, :assigns]
 
+  @type t :: %Without{value: any, result: :ok | :error, assigns: map}
+  @type options :: [assign: atom]
+
+  @type error :: {:error, any()}
+  @type ok :: {:ok, any()}
+  @type result :: error() | ok()
+
+  @type map_func :: (-> result) | (any() -> result) | (any(), map() -> result)
+
+  @spec finit(ok | error | any) :: t
   def finit({:ok, value}), do: %Without{value: value, result: :ok, assigns: %{}}
   def finit({:error, value}), do: %Without{value: value, result: :error, assigns: %{}}
   def finit(value), do: %Without{value: value, result: :ok, assigns: %{}}
 
+  @spec fresult(t) :: {:ok, any} | {:error, any}
   def fresult(%Without{result: result, value: value}) do
     {result, value}
   end
 
+  @spec fmap_ok(any | t, map_func, options) :: t
   def fmap_ok(context, func, opts \\ [])
 
   def fmap_ok(%Without{result: :error} = context, _func, _opts) do
@@ -51,7 +63,8 @@ defmodule Without do
     end
   end
 
-  def fmap_ok(value, func, opts) do
+  def fmap_ok(value, func, opts)
+      when is_function(func, 0) or is_function(func, 1) or is_function(func, 2) do
     value
     |> finit
     |> fmap_ok(func, opts)
@@ -74,6 +87,7 @@ defmodule Without do
     context
   end
 
+  @spec fmap_error(t, map_func) :: t
   def fmap_error(%Without{} = context, func) when is_function(func, 0) do
     case func.() do
       {:ok, value} -> %{context | result: :ok, value: value}
